12bf5923a1ae54fffb78034424f8ceab30bbc0bf
LuisMalte <lcmalte99839@umanizales.edu.co>
2024-03-04 10:12:27 -0500

    4 marzo

--- src/DataBase/connection.js
+++ src/DataBase/connection.js
@@ -1,11 +1,12 @@
-const {Sequelize } = require('sequelize');
-
-var dataBase = 'resturantDB';
+const { Sequelize } = require('sequelize');
+//posible error
+var dataBase = 'restaurantDB';
 var userName = 'postgres';
 var password = '96857412';
 
-const connection = new Sequelize (dataBase, userName, password,{
+const       connection = new Sequelize(dataBase, userName, password,{
     host: 'localhost',
     dialect: 'postgres'
 });
+
 module.exports = connection;
