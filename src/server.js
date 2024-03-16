cf3a50c01a0f029799f69c54c23fecae62c87c30
LuisMalte <lcmalte99839@umanizales.edu.co>
2024-03-15 21:05:51 -0500

    ultima calse

--- src/server.js
+++ src/server.js
@@ -5,30 +5,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 1337;
 
-
-
-
 //routers
-const restaurantrouter = require ('./Routers/restaruantrouters.js')
-const productrouter = require('./Routers/productrouter.js')
+const restaurantrouter = require('./Routers/restaurantrouter.js');
+const productrouter = require('./Routers/productrouter.js');
+const departmentrouter = require('./Routers/departmentrouter.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-connection.sync({force: false})
-    .then(() => { 
-        console.log('Synchronized DataBase'); 
-        app.listen(port, ()=> {
-            console.log("The application is running on port: " + port);
-        })
-    })
-    .catch((error) => { 
-        console.error('Error syncing DataBase' + error);
-    }); 
+app.listen(port, ()=> {
+    console.log("The application is running on port: " + port);
+})
 
 //api
-
-app.use('/api',restaurantrouter)
-app.use('/api',productrouter)
-
-    
+app.use('/api', restaurantrouter);
+app.use('/api', productrouter);
+app.use('/api', departmentrouter);
